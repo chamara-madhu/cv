f8c5a417a1e75331a236078c92ae97259db0043c
Chamara <chamara@spritzer.app>
2021-06-04 22:05:44 +0530

    Updates

---
+++ precache-manifest.6bf341a5f5ae9999ec01213c35e42bc1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417578c4c9bc8c969929b0beed7f6d78",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "731da1eabeb0c21103e2",
+    "url": "/resume/static/css/2.23802283.chunk.css"
+  },
+  {
+    "revision": "9f6a4b76c2596cc1c7e8",
+    "url": "/resume/static/css/main.41866f2b.chunk.css"
+  },
+  {
+    "revision": "731da1eabeb0c21103e2",
+    "url": "/resume/static/js/2.96cc5a4d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/resume/static/js/2.96cc5a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6a4b76c2596cc1c7e8",
+    "url": "/resume/static/js/main.0b415e03.chunk.js"
+  },
+  {
+    "revision": "7426b324e7768b99f472",
+    "url": "/resume/static/js/runtime-main.6086f8f2.js"
+  },
+  {
+    "revision": "32f024b6010f9da4174f3769bfdb489a",
+    "url": "/resume/static/media/agent-dash.32f024b6.jpg"
+  },
+  {
+    "revision": "e5748db433bc9938dbe9129fd393e69e",
+    "url": "/resume/static/media/black-girl.e5748db4.jpg"
+  },
+  {
+    "revision": "f2751dc59065b35996c70e0ee0ddeb95",
+    "url": "/resume/static/media/colorful-elephant.f2751dc5.jpg"
+  },
+  {
+    "revision": "ee08b83659ad6512e3931d9645eeaa03",
+    "url": "/resume/static/media/dissertation.ee08b836.pdf"
+  },
+  {
+    "revision": "b521b442dcca3fec85a4fe89f3b81b31",
+    "url": "/resume/static/media/girl-with-pot-tumb.b521b442.jpg"
+  },
+  {
+    "revision": "7442f2b784550aacfd58125e28af6d4d",
+    "url": "/resume/static/media/girl-with-pot.7442f2b7.jpg"
+  },
+  {
+    "revision": "6241910c3238f5cfa6179b5269cc5997",
+    "url": "/resume/static/media/pix-admin.6241910c.jpg"
+  },
+  {
+    "revision": "9e6da609a70c0f568066d98950667dad",
+    "url": "/resume/static/media/pix.9e6da609.jpg"
+  },
+  {
+    "revision": "09fa84d42ac0ef8290c086f8baeba764",
+    "url": "/resume/static/media/poruwa-admin.09fa84d4.jpg"
+  },
+  {
+    "revision": "867b89ed069f69a4413ed46b5c24d73f",
+    "url": "/resume/static/media/profile-pic.867b89ed.png"
+  },
+  {
+    "revision": "a57a8d6271109d79667c16c417c74b86",
+    "url": "/resume/static/media/ransis-arcade-tumb.a57a8d62.png"
+  },
+  {
+    "revision": "5372750e581c91a9741897eb9eb104c0",
+    "url": "/resume/static/media/ransis-arcade.5372750e.png"
+  },
+  {
+    "revision": "ddc1f3e59e5e9b91f60f586ffd73a1d7",
+    "url": "/resume/static/media/three-elephants.ddc1f3e5.jpg"
+  }
+]);
